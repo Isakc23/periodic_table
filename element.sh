8d95aea361f911510f131a66baf823fec9264668
Isakc23 <cigi2502@gmail.com>
2026-05-23 23:27:02 -0400

    chore: add version comment

--- element.sh
+++ element.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script to search elements in the periodic table database
 # Author: Isakc23
+# Version: 1.0
 
 # This script searches for elements in the periodic table database
 # Script to search elements in the periodic table database
